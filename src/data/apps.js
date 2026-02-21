f53fddca702588e0e858729b273325cc70eb8c42
Brian <briannaz21@hotmail.com>
2026-02-21 11:15:12 -0600

    added themecolor

--- src/data/apps.js
+++ src/data/apps.js
@@ -2,11 +2,13 @@ export const apps = [
   {
     name: "Weather",
     icon: "/icons/weather.png",
-    url: "https://weather-app-brian-naz.vercel.app/"
+    url: "https://weather-app-brian-naz.vercel.app/",
+    themeColor: "#3a7bfd",
   },
   {
-    name: "Chat",
+    name: "Messages",
     icon: "/icons/chat.png",
-    url: "https://messagesappdemo.vercel.app/"
-  }
+    url: "https://messagesappdemo.vercel.app/",
+    themeColor: "#0f0f0f",
+  },
 ];
