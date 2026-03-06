d83214c7aa6096b630d8b2b1793376076750678c
Brian <briannaz21@hotmail.com>
2026-03-06 16:22:08 -0600

    added settings and notes to app drawer

--- src/data/apps.js
+++ src/data/apps.js
@@ -1,14 +1,29 @@
+import Settings from "../settings/Settings";
+
 export const apps = [
   {
     name: "Weather",
     icon: "/icons/weather.png",
     url: "https://weather-app-brian-naz.vercel.app/",
-    themeColor: "#3a7bfd",
+    supportsTheme: false,
   },
   {
     name: "Messages",
     icon: "/icons/chat.png",
     url: "https://messagesappdemo.vercel.app/",
-    themeColor: "#0f0f0f",
+    supportsTheme: false,
+  },
+  {
+    name: "Notes",
+    icon: "/icons/notes.png",
+    url: "http://notes-app-azure-nine.vercel.app/",
+    supportsTheme: false,
+  },
+  {
+    name: "Settings",
+    icon: "/icons/settings.png",
+    type: "internal",
+    component: Settings,
+    supportsTheme: true
   },
 ];
